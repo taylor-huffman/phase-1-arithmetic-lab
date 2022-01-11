f811eb65003b3666ecb87b79d53f709067af0066
Taylor Huffman <taylor@crucialartists.com>
2022-01-10 21:43:50 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random());
+
+const num3 = 16;
+const num4 = 4;
+const mod = num3 / num4;
+
+const max = Math.max(1, 2, 4, 6, 10, 15, 20);
